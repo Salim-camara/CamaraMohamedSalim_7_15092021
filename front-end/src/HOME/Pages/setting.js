db0bc925b0bdd0fec3c75ae5c079f1a3cd6a5cbd
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-09-27 00:19:20 +0200

    Ajout de la page de profil user

--- front-end/src/HOME/Pages/setting.js
+++ front-end/src/HOME/Pages/setting.js
@@ -14,20 +14,24 @@ const Setting = () => {
                 {/* partie PdP */}
                 <div className="left">
                     <img src={ PdP } className="left--pdp" />
+                    <div className="left__age">
+                        <p className="left__age--p">21 ans</p>
+                        <i class="fas fa-mars left__age--icon"></i>
+                    </div>
                 </div>
 
                 {/* Partie description */}
                 <div className="right">
 
                     <div className="right__title">
-                        <h1>Lamasticot</h1>
+                        <h1 className="right__title--h1">Lamasticot</h1>
                     </div>
                     <div className="right__bio">
                         <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
                     </div>
-                    <div className="right__age">
-                        <p>21 ans</p>
-                        <i class="fas fa-mars"></i>
+                    <div className="right__button">
+                        <button className="right__button--modify button">Modifier</button>
+                        <button className="right__button--delete button">Supprimer</button>
                     </div>
 
                 </div>
